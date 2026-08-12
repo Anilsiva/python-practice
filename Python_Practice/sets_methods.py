7fe225091e6af583c5804bf571b930cdfafa3d33
Anilprasad Achutuni <80614126+Anilsiva@users.noreply.github.com>
2026-08-12 12:34:54 +0000

    Python code practising in vscode

---
+++ Python_Practice/sets_methods.py
@@ -0,0 +1,23 @@
+# Sets Intersection: Write Python code to find and print the intersection of the following two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+set3 = set1.intersection(set2)
+print(set3)
+
+# Sets Union: Write Python code to find and print the union of the following two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+set3 = set1.union(set2)
+print(set3)
+
+# Set Difference: Write Python code to find and print the elements present in set1 but not in set2
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8} 
+set3 = set1.difference(set2)
+print(set3)
+
+# Symmetric Difference: Write Python code to find and print the symmetric difference of the following two sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+set3 = set1.symmetric_difference(set2)
+print(set3)
